0b1013aafec7baa01a4ffd557a13534a6a60ec4f
Vladislav Lukmanov <arhimed89@mail.ru>
2018-10-13 14:36:42 +0300

    feature/BA-0001 add basic spring project structure

---
+++ src/main/java/io/khasang/ba/controller/AppController.java
@@ -0,0 +1,21 @@
+package io.khasang.ba.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+    @RequestMapping("/")
+    public String getHelloPage(Model model) {
+        model.addAttribute("name", "First Spring application!");
+        return "home";
+    }
+
+    @RequestMapping("/name/{name}")
+    public String getName(@PathVariable("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
